5d7daa3ef8e595f792cded7e7864fd02e58b82ac
Riyakuila <riyakuila1006@gmail.com>
2025-08-17 18:18:09 +0530

    Final output

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const SEND_API = "/api/send";
 export const VIEW_API = "/api/view";
-export const REPO_LINK = "https://github.com/Debjyoti2004/CryptDrop.git";
+export const REPO_LINK = "https://github.com/Riyakuila/SecretPetals";
